c5f095a6e064769999d900f17ec65c834d3ac358
Finn Walker <finn3walker@gmail.com>
2019-11-27 17:43:01 +0000

    fixed scheme

--- dnsw.js
+++ dnsw.js
@@ -6,17 +6,9 @@ mongoose.Promise = global.Promise;
 const Schema = mongoose.Schema;
 
 const ParticipantSchema = new Schema({
-    eligible: Boolean,
-    terms: Boolean,
-    first_name: String,
-    last_name: String,
-    date_of_birth: String,
-    email: String,
-    mobile_number: String,
-    marketing_tab: Boolean,
-    marketing_atc: Boolean,
-    active: Boolean,
-    unsubscribed: false
+    player_name: String,
+    home_team: String,
+    away_team: String
 });
 
 const ParticipantModel = mongoose.model("Participant", ParticipantSchema);
