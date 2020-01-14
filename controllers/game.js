493f9b8db77c014ed68dce907e26055b333a6127
FinnWalker <finn3walker@gmail.com>
2020-01-14 23:02:36 +0000

    new envs

--- controllers/game.js
+++ controllers/game.js
@@ -9,8 +9,8 @@ require("dotenv").config();
 
 const crypto = require("crypto");
 const algorithm = "aes-256-cbc";
-const key = crypto.scryptSync(process.env.ENCRYPTION_KEY, "salt", 32);
-const iv = crypto.scryptSync(process.env.ENCRYPTION_KEY, "salt", 16);
+const key = Buffer.from(process.env.ENCRYPTION_KEY, 'hex');
+const iv = Buffer.from(process.env.ENCRYPTION_IV, 'hex');
 
 function decrypt(text) {
   let decipher = crypto.createDecipheriv(algorithm, key, iv);
