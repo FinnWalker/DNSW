7ad6e7b364e26e61aa4775fe24575ee1e111001e
FinnWalker <finn3walker@gmail.com>
2020-01-14 21:03:44 +0000

    test

--- controllers/game.js
+++ controllers/game.js
@@ -5,34 +5,27 @@ const formidable = require("formidable");
 //const path = require("path");
 const nodemailer = require("nodemailer");
 
-require("dotenv").config();
+require('dotenv').config();
 
-const crypto = require("crypto");
-const algorithm = "aes-256-cbc";
-const key = crypto.scryptSync(process.env.TEST, "salt", 32);
-const iv = crypto.scryptSync(process.env.TEST, "salt", 16);
-
-function decrypt(text) {
+const crypto = require('crypto');
+const algorithm = 'aes-256-cbc';
+const key = crypto.scryptSync(process.env.TEST, 'salt', 32);
+const iv = crypto.scryptSync(process.env.TEST, 'salt', 16);
+ 
+ function decrypt(text) {
   let decipher = crypto.createDecipheriv(algorithm, key, iv);
-  let decrypted = decipher.update(text, "hex");
+  let decrypted = decipher.update(text, 'hex');
   decrypted += decipher.final();
   return decrypted.toString();
-}
+ }
 
-participantModel.findOne({ player_name: "Finn" }, (err, participant) => {
-  if (err) console.log(err);
-  else if (participant) {
-    //console.log("participant found");
-    //console.log(participant);
+participantModel.findOne({player_name:"Finn"}, (err, participant) => {
+  if(err) console.log(err)
+  else if(participant) {
     participant.email = "finn3walker@gmail.com";
-    participant.save().then(() => {
-      //console.log("participant changed");
-      //console.log(participant);
-    });
-  } else {
-    console.log("fuck");
+    participant.save();
   }
-});
+})
 
 async function email(email, name, image_path) {
   let transporter = nodemailer.createTransport({
@@ -122,7 +115,7 @@ module.exports = {
             playerName: participant.player_name,
             homeTeam: participant.home_team,
             awayTeam: participant.away_team,
-            topScore: participant.top_score.toString()
+            topScore: participant.top_score.toString(),
             //email: participant.email
           };
           players.push(player);
@@ -156,6 +149,7 @@ module.exports = {
       if (err) {
         console.log(err);
       } else if (files.image && fields.playerName) {
+
         const image_path = files.image.path;
 
         const player_name = sanitize(fields.playerName);
@@ -170,19 +164,16 @@ module.exports = {
           if (err) throw err;
         });
         */
-        participantModel.findOne(
-          { player_name: fields.playerName },
-          (err, participant) => {
-            if (participant) {
-              //console.log(decrypt(participant.email));
-              console.log(participant.email);
-              email(participant.email, player_name, image_path);
-              res.json({ participant });
-            } else {
-              res.status(400).json({ message: "Player does not exist" });
-            }
+        participantModel.findOne({ player_name:fields.playerName }, (err, participant) => {
+          if (participant) {
+            
+            email(decrypt(participant.email), player_name, image_path);
+            res.json({ participant });
+          } else {
+            res.status(400).json({ message: "Player does not exist" });
           }
-        );
+        });
+       
       } else {
         res.status(300).json({ message: "Please include all fields" });
       }
