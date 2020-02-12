310144b86a2004538723d5a88714ae4637f91771
FinnWalker <finn3walker@gmail.com>
2020-02-12 18:31:25 +0000

    test

--- controllers/game.js
+++ controllers/game.js
@@ -115,8 +115,7 @@ module.exports = {
         for (let participant of participants) {
           let best_score = 0;
           for (let score in participant.scores) {
-            console.log(score);
-            if (score.score > best_score) best_score = score.score;
+            if (score > best_score) best_score = score;
           }
           let player = {
             playerName: participant.player_name,
